1b99d083318a3e11ca89ed77aff8625dcb08b4a9
jonny sywulak <jonny@stelligent.com>
2015-07-27 12:50:47 -0700

    pending pending pending

--- features/stepdefs/hambuger_steps.rb
+++ features/stepdefs/hambuger_steps.rb
@@ -1,5 +1,5 @@
 require 'aws-sdk-core'
-#require_relative '../../lib/hamburger.rb'
+# require_relative '../../lib/hamburger.rb'
 
 Given(/^test data to use$/) do
   @key = "testkey#{Time.now.strftime '%Y%m%d%H%M%S'}"
@@ -21,3 +21,82 @@ Given(/^a DynamoDB table to use$/) do
   fail if @table_name.nil?
 end
 
+When(/^I store a value in the keystore using the API$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see that encrypted data in the raw data store$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I retrieve a value from the keystore using the API$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should get that data back in plaintext$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I retrieve all values from the data store using the API$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should get back a hash of all the values$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I store a value in the keystore using the CLI$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I retrieve a value from the keystore using the CLI$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I retrieve all values from the data store using the CLI$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should get back a JSON document of all the values$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I try to retrieve a value using the wrong KMS key$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should get an error that tells me I was using the wrong key\.$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I try to retrieve a value that does not exist$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should get an error that tells me that the value does not exist$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I try to retrieve a value for a non\-existent parameter name from the API$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should recieve an nil value$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I try to retrieve a value for a non\-existent parameter name from the CLI$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should recieve an empty string$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I try to retrieve a value from a store that does not exist$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should get an error that tells me that the store does not exist\.$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
